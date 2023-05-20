59e361f2a0d33030114a548039a46966158984ca
Raphael Alexsandro <49734953+RaphaelSBarros@users.noreply.github.com>
2023-05-19 21:42:48 -0300

    Add files via upload

---
+++ Exemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exemplo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Novo Exemplo");
+            Console.WriteLine("Digite um numero: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite um numero: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            int resultado = Somar(numero1, numero2);
+            Console.WriteLine(resultado);
+
+            
+        }
+
+        static int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
